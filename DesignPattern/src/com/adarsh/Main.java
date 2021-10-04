c74badcd8e3ae165c9fc4869acf277c61c81b02a
Adarsh Kumar Verma <adarshverma013@gmail.com>
2021-10-05 00:59:23 +0530

    Memento Pattern added

---
+++ DesignPattern/src/com/adarsh/Main.java
@@ -0,0 +1,25 @@
+package com.adarsh;
+
+import com.adarsh.memento.Editor;
+import com.adarsh.memento.History;
+
+public class Main {
+    public static void main(String[] args) {
+        //memento pattern
+        /* var editor = new Editor();
+        var history = new History();
+        editor.setContent("a");
+        history.push(editor.createState());
+        editor.setContent("b");
+        history.push(editor.createState());
+        editor.setContent("c");
+        history.push(editor.createState());
+
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
+        */
+
+    }
+}
